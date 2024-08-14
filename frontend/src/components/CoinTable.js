98f4b38afec66cde274c4ddd62193decb3d6c99b
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-08-14 21:58:22 +0300

    Nati/style table (#14)
    * add paging to the table, style the search, add header to the table
    
    * styling table
    
    * fixing rank for all pages
    
    ---------
    
    Co-authored-by: Adar Budomski <adar688@gmail.com>

--- frontend/src/components/CoinTable.js
+++ frontend/src/components/CoinTable.js
@@ -1,105 +1,224 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Coin from '../components/coin';  // Ensure the import matches the actual file name
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/coinTable.css";
+import "../styles/coin.css";
+import { formatPrice } from "../components/utils";
 
 function CoinTable() {
-    const [coins, setCoins] = useState([]);
-    const [search, setSearch] = useState('');
-    const [sortField, setSortField] = useState('name');
-    const [sortOrder, setSortOrder] = useState('asc');
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortField, setSortField] = useState("name");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [totalMarketCap, setTotalMarketCap] = useState(0); // useState for total number of market cap
+  const [currentPage, setCurrentPage] = useState(1); //useState for current page in the table
+  const coinsPerPage = 20; //set num og coins in the table per page
 
-    useEffect(() => {
-        axios
-            .get(
-                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-            )
-            .then(res => {
-                setCoins(res.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        const coinsData = res.data;
+        setCoins(coinsData);
 
-    const handleChange = e => {
-        setSearch(e.target.value);
-    };
+        // Compute total market cap
+        const totalCap = coinsData.reduce(
+          (sum, coin) => sum + coin.market_cap,
+          0
+        );
+        setTotalMarketCap(totalCap);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    const handleSort = field => {
-        const newSortOrder = sortField === field && sortOrder === 'asc' ? 'desc' : 'asc';
-        const sortedCoins = [...coins].sort((a, b) => {
-            if (field === 'name' || field === 'symbol') {
-                return newSortOrder === 'asc'
-                    ? a[field].localeCompare(b[field])
-                    : b[field].localeCompare(a[field]);
-            } else {
-                return newSortOrder === 'asc'
-                    ? a[field] - b[field]
-                    : b[field] - a[field];
-            }
-        });
-        setSortField(field);
-        setSortOrder(newSortOrder);
-        setCoins(sortedCoins);
-    };
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const filteredCoins = coins.filter(coin =>
-        coin.name.toLowerCase().includes(search.toLowerCase())
-    );
+  const handleSort = (field) => {
+    const newSortOrder =
+      sortField === field && sortOrder === "asc" ? "desc" : "asc";
+    const sortedCoins = [...coins].sort((a, b) => {
+      if (field === "name" || field === "symbol") {
+        return newSortOrder === "asc"
+          ? a[field].localeCompare(b[field])
+          : b[field].localeCompare(a[field]);
+      } else {
+        return newSortOrder === "asc"
+          ? a[field] - b[field]
+          : b[field] - a[field];
+      }
+    });
+    setSortField(field);
+    setSortOrder(newSortOrder);
+    setCoins(sortedCoins);
+  };
 
-    return (
-        <div className='coin-app'>
-            <div className='coin-search'>
-                <h1 className='coin-text'>Search a currency</h1>
-                <input
-                    className='coin-input'
-                    type='text'
-                    onChange={handleChange}
-                    placeholder='Search'
-                />
-            </div>
-            <table className='coin-table'>
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th className={`sortable ${sortField === 'name' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('name')}>
-                            Name
-                        </th>
-                        <th className={`sortable ${sortField === 'symbol' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('symbol')}>
-                            Symbol
-                        </th>
-                        <th className={`sortable ${sortField === 'current_price' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('current_price')}>
-                            Price
-                        </th>
-                        <th className={`sortable ${sortField === 'market_cap' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('market_cap')}>
-                            Market Cap
-                        </th>
-                        <th className={`sortable ${sortField === 'total_volume' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('total_volume')}>
-                            Volume
-                        </th>
-                        <th className={`sortable ${sortField === 'price_change_percentage_24h' ? `sorted-${sortOrder}` : ''}`} onClick={() => handleSort('price_change_percentage_24h')}>
-                            Price Change
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredCoins.map(coin => (
-                        <tr key={coin.id}>
-                            <td><img src={coin.image} alt={coin.name} style={{ width: '30px' }} /></td>
-                            <td>{coin.name}</td>
-                            <td>{coin.symbol}</td>
-                            <td>${coin.current_price}</td>
-                            <td>${coin.market_cap.toLocaleString()}</td>
-                            <td>${coin.total_volume.toLocaleString()}</td>
-                            <td className={coin.price_change_percentage_24h < 0 ? 'red' : 'green'}>
-                                {coin.price_change_percentage_24h.toFixed(2)}%
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+  const getSortIcon = (field) => {
+    if (sortField === field) {
+      return sortOrder === "asc" ? "▲" : "▼";
+    }
+    return "⇅";
+  };
+
+  //change the current table page
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const totalPages = Math.ceil(filteredCoins.length / coinsPerPage); //calculate num of pages of the table
+
+  const currentCoins = filteredCoins.slice(
+    (currentPage - 1) * coinsPerPage,
+    currentPage * coinsPerPage
+  );
+
+  return (
+    <div className="coin-app">
+      <div className="coin-header">Largest Coins by Market Cap</div>
+      <div className="coin-summary">
+        Total Coins: {filteredCoins.length} | Total Market Cap: $
+        {formatPrice(totalMarketCap)}
+      </div>
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency by name</h1>
+        <input
+          className="coin-input"
+          type="text"
+          onChange={handleChange}
+          placeholder="Search by name"
+        />
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <div
+          className="d-flex justify-content-center shadow-lg pt-3 mt-5 pb-3 bg-white rounded"
+          style={{ width: "95%" }}
+        >
+          <table id="coin-table" className="table table-hover">
+            <thead className="">
+              <tr>
+                <th>Rank</th>
+                <th
+                  className={`sortable ${
+                    sortField === "name" ? `sorted-${sortOrder}` : ""
+                  }`}
+                  onClick={() => handleSort("name")}
+                >
+                  {getSortIcon("name")} Name
+                </th>
+                <th
+                  className={`sortable ${
+                    sortField === "symbol" ? `sorted-${sortOrder}` : ""
+                  }`}
+                  onClick={() => handleSort("symbol")}
+                >
+                  {getSortIcon("symbol")} Symbol
+                </th>
+                <th
+                  className={`sortable ${
+                    sortField === "current_price" ? `sorted-${sortOrder}` : ""
+                  }`}
+                  onClick={() => handleSort("current_price")}
+                >
+                  {getSortIcon("current_price")} Price
+                </th>
+                <th
+                  className={`sortable ${
+                    sortField === "market_cap" ? `sorted-${sortOrder}` : ""
+                  }`}
+                  onClick={() => handleSort("market_cap")}
+                >
+                  {getSortIcon("market_cap")} Market Cap
+                </th>
+                <th
+                  className={`sortable ${
+                    sortField === "total_volume" ? `sorted-${sortOrder}` : ""
+                  }`}
+                  onClick={() => handleSort("total_volume")}
+                >
+                  {getSortIcon("total_volume")} Volume
+                </th>
+                <th
+                  className={`sortable ${
+                    sortField === "price_change_percentage_24h"
+                      ? `sorted-${sortOrder}`
+                      : ""
+                  }`}
+                  onClick={() => handleSort("price_change_percentage_24h")}
+                >
+                  {getSortIcon("price_change_percentage_24h")} Today
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentCoins.map((coin, index) => (
+                <tr key={coin.id}>
+                  <td>{(currentPage - 1) * coinsPerPage + (index + 1)}</td>
+                  <td>
+                    <img
+                      src={coin.image}
+                      alt={coin.name}
+                      style={{ width: "30px" }}
+                    />
+                    &nbsp;&nbsp;{coin.name}
+                  </td>
+                  <td>{coin.symbol.toUpperCase()}</td>
+                  <td>${formatPrice(coin.current_price)}</td>
+                  <td>${formatPrice(coin.market_cap)}</td>
+                  <td>${formatPrice(coin.total_volume)}</td>
+                  <td
+                    className={
+                      coin.price_change_percentage_24h < 0
+                        ? "text-danger"
+                        : "text-success"
+                    }
+                  >
+                    {coin.price_change_percentage_24h < 0 ? "▼" : "▲"}{" "}
+                    {coin.price_change_percentage_24h.toFixed(2)}%
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="pagination-button"
+        >
+          &#9664; {/* Left arrow */}
+        </button>
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={`pagination-button ${
+              currentPage === index + 1 ? "active" : ""
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="pagination-button"
+        >
+          &#9654; {/* Right arrow */}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CoinTable;
--- frontend/src/pages/contactus.js
+++ frontend/src/pages/contactus.js
@@ -1,121 +1,157 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { Container, Row, Col, Form, Button, Alert, Card } from 'react-bootstrap';
-import '../styles/contactus.css';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Alert,
+  Card,
+} from "react-bootstrap";
+import "../styles/contactus.css";
 
 const ContactUs = () => {
-    return (
-        <Container className="mt-4">
-            <Row>
-                <Col md={6}>
-                    <ContactForm />
-                </Col>
-                <Col md={6}>
-                    <ContactInfo />
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col md={6}>
+          <ContactForm />
+        </Col>
+        <Col md={6}>
+          <ContactInfo />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 const ContactForm = () => {
-    const form = useRef();
+  const form = useRef();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const name = e.target.name.value;
-        const email = e.target.email.value;
-        const info = e.target.info.value;
-        const predefinedSubject = e.target.predefinedSubject.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const info = e.target.info.value;
+    const predefinedSubject = e.target.predefinedSubject.value;
 
-        if (name === '' || email === '' || info === '' || predefinedSubject === '') {
-            alert('All fields are required!');
-        } else if (!validateEmail(email)) {
-            alert('Please enter a valid email address.');
-        } else {
-            sendEmail(e);
+    if (
+      name === "" ||
+      email === "" ||
+      info === "" ||
+      predefinedSubject === ""
+    ) {
+      alert("All fields are required!");
+    } else if (!validateEmail(email)) {
+      alert("Please enter a valid email address.");
+    } else {
+      sendEmail(e);
+    }
+  };
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const sendEmail = (e) => {
+    emailjs
+      .sendForm(
+        "service_5rbpg5k",
+        "template_ufdu9pe",
+        form.current,
+        "Nt-NeJLewxMEiJHyK"
+      )
+      .then(
+        () => {
+          alert(
+            `Thank you, ${e.target.name.value}. Your message has been sent!`
+          );
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          alert("An error occurred while sending the email.");
         }
-    };
+      );
+  };
 
-    const validateEmail = (email) => {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(String(email).toLowerCase());
-    };
+  const handleSupportClick = () => {
+    alert("Support Contact: support@example.com");
+  };
 
-    const sendEmail = (e) => {
-        emailjs.sendForm('service_5rbpg5k', 'template_ufdu9pe', form.current, 'Nt-NeJLewxMEiJHyK')
-            .then(
-                () => {
-                    alert(`Thank you, ${e.target.name.value}. Your message has been sent!`);
-                },
-                (error) => {
-                    console.log('FAILED...', error.text);
-                    alert('An error occurred while sending the email.');
-                }
-            );
-    };
-
-    const handleSupportClick = () => {
-        alert('Support Contact: support@example.com');
-    };
-
-    return (
-        <Form ref={form} id="contactForm" onSubmit={handleSubmit}>
-            <h2>Contact Us</h2>
-            <Form.Group controlId="name">
-                <Form.Label>Name <span className="text-danger">*</span></Form.Label>
-                <Form.Control type="text" name="name" placeholder="Name" required />
-            </Form.Group>
-            <Form.Group controlId="email">
-                <Form.Label>Email <span className="text-danger">*</span></Form.Label>
-                <Form.Control type="email" name="email" placeholder="Email" required />
-            </Form.Group>
-            <Form.Group controlId="info">
-                <Form.Label>Info <span className="text-danger">*</span></Form.Label>
-                <Form.Control type="text" name="info" placeholder="Info" required />
-            </Form.Group>
-            <Form.Group controlId="predefinedSubject">
-                <Form.Label>Subject <span className="text-danger">*</span></Form.Label>
-                <Form.Control as="select" name="predefinedSubject" required>
-                    <option value="">Select a subject</option>
-                    <option value="General Inquiry">General Inquiry</option>
-                    <option value="Technical Support">Technical Support</option>
-                    <option value="Billing">Billing</option>
-                </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit" className="mt-3 ms-2">
-                Submit
-            </Button>
-            <Button variant="secondary" type="button" className="mt-3 ms-2" onClick={handleSupportClick}>
-                Contact Support
-            </Button>
-        </Form>
-    );
+  return (
+    <Form ref={form} id="contactForm" onSubmit={handleSubmit}>
+      <h2>Contact Us</h2>
+      <Form.Group controlId="name">
+        <Form.Label>
+          Name <span className="text-danger">*</span>
+        </Form.Label>
+        <Form.Control type="text" name="name" placeholder="Name" required />
+      </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Label>
+          Email <span className="text-danger">*</span>
+        </Form.Label>
+        <Form.Control type="email" name="email" placeholder="Email" required />
+      </Form.Group>
+      <Form.Group controlId="info">
+        <Form.Label>
+          Info <span className="text-danger">*</span>
+        </Form.Label>
+        <Form.Control type="text" name="info" placeholder="Info" required />
+      </Form.Group>
+      <Form.Group controlId="predefinedSubject">
+        <Form.Label>
+          Subject <span className="text-danger">*</span>
+        </Form.Label>
+        <Form.Control as="select" name="predefinedSubject" required>
+          <option value="">Select a subject</option>
+          <option value="General Inquiry">General Inquiry</option>
+          <option value="Technical Support">Technical Support</option>
+          <option value="Billing">Billing</option>
+        </Form.Control>
+      </Form.Group>
+      <Button variant="primary" type="submit" className="mt-3 ms-2">
+        Submit
+      </Button>
+      <Button
+        variant="secondary"
+        type="button"
+        className="mt-3 ms-2"
+        onClick={handleSupportClick}
+      >
+        Contact Support
+      </Button>
+    </Form>
+  );
 };
 
 const ContactInfo = () => {
-    return (
-        <Card className="text-center mt-4">
-            <Card.Body>
-                <Card.Title>Contact Us</Card.Title>
-                <Card.Text>If you have any questions, feel free to reach out to us!</Card.Text>
-                <div className="justify-content-center flex-wrap">
-                    <div className="p-2">
-                        <h4>Email Us:</h4>
-                        <p>support@example.com</p>
-                    </div>
-                    <div className="p-2">
-                        <h4>Call Us:</h4>
-                        <p>+1 234 567 890</p>
-                    </div>
-                </div>
-                <Alert variant="info" className="mt-3">
-                    <h4>Special Offer!</h4>
-                    <p>Get 20% off on your first purchase. Use code: FIRST20</p>
-                </Alert>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className="text-center mt-4">
+      <Card.Body>
+        <Card.Title>Contact Us</Card.Title>
+        <Card.Text>
+          If you have any questions, feel free to reach out to us!
+        </Card.Text>
+        <div className="justify-content-center flex-wrap">
+          <div className="p-2">
+            <h4>Email Us:</h4>
+            <p>support@example.com</p>
+          </div>
+          <div className="p-2">
+            <h4>Call Us:</h4>
+            <p>+1 234 567 890</p>
+          </div>
+        </div>
+        <Alert variant="info" className="mt-3">
+          <h4>Special Offer!</h4>
+          <p>Get 20% off on your first purchase. Use code: FIRST20</p>
+        </Alert>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default ContactUs;
